00c5cb9db58b3a7a3ddc8999d4e256ac2ad337dd
Jokku <jokkuskattakkayam@gmail.com>
2019-01-19 11:10:08 -0500

    completed

---
+++ SportsStoreServices/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using SportsStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStoreServices
+{
+   public class ProductService
+    {
+        private ApplicationDbContext _context;
+
+        public ProductService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Product> GetAll()
+        {
+            return _context.Products;
+        }
+
+        public Product GetById(int Id)
+        {
+            return GetAll().FirstOrDefault(a => a.ProductID == Id);
+        }
+    }
+}
---
+++ SportsStoreServices/ReviewService.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using SportsStore.Models;
+using System.Linq;
+
+namespace SportsStoreServices
+{
+    public class ReviewService
+    {
+        private ApplicationDbContext _context;
+
+        public ReviewService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Review> GetAll()
+        {
+            return _context.Reviews;
+        }
+
+        public void Add(Review rev)
+        {
+            _context.Add(rev);
+            _context.SaveChanges();
+        }
+
+        public Review GetComment(int Id)
+        {
+           return _context.Reviews.FirstOrDefault(a => a.Id == Id);
+        }
+
+        public IEnumerable<Review> GetByProduct(Product p)
+        {
+            return GetAll().Where(a => a.product == p);
+        }
+    }
+}
